4d0d98cc0caac2456e601136c5bc7840a48e09ac
DIYA-4 <diyahnytomar8171@gmail.com>
2026-07-17 23:09:10 +0530

    Delete powandar.py

--- powandar.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a number:"))
-p = int(input("Enter the power:"))
-
-print("Power=", n*p)
-
-r = int(input("Enter the radius:"))
-area = 3.14 * r *r
-print("Area of circle =", area)
